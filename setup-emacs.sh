ce82bb3bb1ab16fdd4853998f12c5e89da857ff1
Daniel Lawrence <Daniel Lawrence>
2013-05-21 23:05:21 +1000

    Added evil to emacs

--- setup-emacs.sh
+++ setup-emacs.sh
@@ -6,3 +6,10 @@
 source libfunc
 install_ppa ppa:cassou/emacs
 install_package emacs24 emacs24-el emacs24-common-non-dfsg
+
+# install evil
+if [ ! -d ~/.emacs.d/evil ];then
+	(cd ~/.emacs.d/;git clone git://gitorious.org/evil/evil.git)
+fi
+
+
