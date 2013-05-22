74ef76fc5a92f0d03a3410b869436504afe71ab2
Daniel Lawrence <Daniel Lawrence>
2013-05-22 18:56:26 +1000

    Fixed email

--- setup-git.sh
+++ setup-git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install git
-git config --global user.email dannyla@linux.com1
+git config --global user.email dannyla@linux.com
 git config --global user.email 'Daniel Lawrence'
